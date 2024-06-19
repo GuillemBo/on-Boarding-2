9ecb2d2d1ebc83f5aa2e1c2eaff75175c5c239b8
Guillem <guillemb4b@gmail.com>
2024-06-19 13:28:57 +0200

    Animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { EscenaComponent } from './escena/escena.component'
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, EscenaComponent, CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'onBoarding';
+
 }
--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -3,16 +3,51 @@ import { iStep } from './../interfaces/i-step.interface';
 import { Component, Input } from '@angular/core';
 import { HomeComponent } from '../home/home.component';
 
+import { trigger, transition, query, style, animate, group } from '@angular/animations';
+
+const left = [
+  query(':enter, :leave', style({ position: 'fixed', width: '200px' }), { optional: true }),
+  group([
+    query(':enter', [style({ transform: 'translateX(-200px)' }), animate('.3s ease-out', style({ transform: 'translateX(0%)' }))], {
+      optional: true,
+    }),
+    query(':leave', [style({ transform: 'translateX(0%)' }), animate('.3s ease-out', style({ transform: 'translateX(200px)' }))], {
+      optional: true,
+    }),
+  ]),
+];
+
+const right = [
+  query(':enter, :leave', style({ position: 'fixed', width: '200px' }), { optional: true }),
+  group([
+    query(':enter', [style({ transform: 'translateX(200px)' }), animate('.3s ease-out', style({ transform: 'translateX(0%)' }))], {
+      optional: true,
+    }),
+    query(':leave', [style({ transform: 'translateX(0%)' }), animate('.3s ease-out', style({ transform: 'translateX(-200px)' }))], {
+      optional: true,
+    }),
+  ]),
+];
+
 @Component({
   selector: 'app-escena',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HomeComponent],
   templateUrl: './escena.component.html',
-  styleUrl: './escena.component.scss'
+  styleUrl: './escena.component.scss',
+  animations: [
+    trigger('animImageSlider', [
+      transition(':increment', right),
+      transition(':decrement', left),
+    ]),
+  ]
 })
+
+
 export class EscenaComponent {
   @Input() steps: iStep[] = [];
 
+  imgSrc: string = 'assets/time_managment.svg';
 
   currentStep: number = 0;
 
@@ -28,4 +63,13 @@ export class EscenaComponent {
     }
   }
 
+  goToStep(index: number) {
+    this.currentStep = index
+  }
+
+  changeImg(index: number) {
+    if (index === 0) this.imgSrc = 'assets/time_managment.svg';
+    if (index === 1) this.imgSrc = 'assets/programming.svg'
+    if (index === 2) this.imgSrc = 'assets/meditation.svg'
+  }
 }
